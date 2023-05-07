d8b33c50b5064abf3c6b8ee1fe3e9b6c6256bdf7
giorgisilagadze <silagadze.gio123@gmail.com>
2023-05-07 15:03:38 +0400

    style: create global style and configure colours

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="w-full h-full bg-wholeDark"></div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        wholeWhite: "#FFFFFF",
+        wholeDark: "#050505",
+        switchOff: "#757575",
+        switchOn: "#A445ED",
+        input: "#F4F4F4",
+        play: "#A445ED",
+        inputDark: "#1F1F1F",
+      },
+      colors: {
+        txtOnWhite: "#2D2D2D",
+        dim: "#757575",
+        violet: "#A445ED",
+        txtOnDark: "#FFFFFF"
+      }
+    },
+  },
+  plugins: [],
+}
+
